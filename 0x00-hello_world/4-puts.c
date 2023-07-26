3f06782382525c0d5bdbe3d7e682a6e354c1bc8c
Jibril4971 <mohammedjibril4971@gmail.com>
2023-07-26 09:32:23 -0700

    main.h

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/**
+ * main. A program that print a line with puts function
+ * Return 0;
+ *\
+ int main(void)
+ { put ("Programming is like building a multilingual puzzle")
+ return (o); }
